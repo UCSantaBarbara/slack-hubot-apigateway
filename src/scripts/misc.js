33506f807eeb57bfa2a22eb7564469bd41dc43c9
Kevin Wu <kevin.wu@ucsb.edu>
2019-03-04 16:26:32 -0800

    try/catch git repo info

--- src/scripts/misc.js
+++ src/scripts/misc.js
@@ -6,8 +6,12 @@ const Git = require('nodegit')
 module.exports = robot => {
   //returns the current git sha hash, useful for determining what version is running in production
   robot.respond(/misc version/i, async res => {
-    const currentRepo = await Git.Repository.open(process.cwd())
-    const head = await currentRepo.getHeadCommit()
+    try {
+      const currentRepo = await Git.Repository.open(process.cwd())
+      const head = await currentRepo.getHeadCommit()
+    } catch (err) {
+      console.error('unable to get git repo information', err)
+    }
 
     res.send(
       `Hubot is running version \`${head.sha()}\`` +
